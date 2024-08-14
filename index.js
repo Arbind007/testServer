a8d80bd12cc078cf647808b234914eafa7f34709
ARBIND LOCHAN MISHRA <almbbsr456@gmail.com>
2024-08-15 00:42:17 +0530

    bug fixes

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const dataSchema = new mongoose.Schema({
     content: mongoose.Schema.Types.Mixed,
     createdAt: {
         type: Date,
-        default: Date.now
+        
+        default: () => {
+          const now = new Date();
+          const istOffset = 5.5 * 60 * 60 * 1000;
+          return new Date(now.getTime() + istOffset);
+      }
     }
 });
 
